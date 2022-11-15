d9de8ace90ad2a5d304a5244c278a3172614d8bf
SamuelOI <95894328+blackkiverson@users.noreply.github.com>
2022-11-15 09:57:46 +0100

    trying to get file download working

--- pages/index.js
+++ pages/index.js
@@ -44,7 +44,7 @@ export default function Home() {
                 <a
                   className="bg-gradient-to-r from-cyan-500 to-teal-500
                 text-white px-4 py-2 rounded-md ml-8"
-                  href="../public/CV[9317].pdf" download
+                  href='../public/CV[9317].pdf' download="Samuel's Resume PDF"
                 >
                   Resume
                 </a>
