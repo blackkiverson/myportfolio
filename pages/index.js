ee372292676d072fc1cec18abf804e0f0ea10a96
SamuelOI <95894328+blackkiverson@users.noreply.github.com>
2022-11-17 11:38:32 +0100

    resume download button fixed

--- pages/index.js
+++ pages/index.js
@@ -44,7 +44,7 @@ export default function Home() {
                 <a
                   className="bg-gradient-to-r from-cyan-500 to-teal-500
                 text-white px-4 py-2 rounded-md ml-8"
-                  href='../public/resume.pdf' download="Samuel's Resume PDF"
+                  href='resume.pdf' download="Samuel's Resume PDF"
                 >
                   Resume
                 </a>
