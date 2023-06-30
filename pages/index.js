222662138c4fd321fb212882089d994ce2f7f02e
SamuelOI <95894328+blackkiverson@users.noreply.github.com>
2023-06-30 12:10:33 +0100

    changed resume

--- pages/index.js
+++ pages/index.js
@@ -138,7 +138,7 @@ export default function Home() {
                   <a
                     className="bg-gradient-to-r from-cyan-500 to-teal-500
                 text-white px-4 py-2 rounded-md ml-8 dark:text-black"
-                    href="new_resume.pdf"
+                    href="Samuel's Resume PDF.pdf"
                     download="Samuel's Resume PDF"
                   >
                     Resume
